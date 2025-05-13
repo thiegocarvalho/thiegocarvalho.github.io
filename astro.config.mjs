bfc8ffeac202931244c0aaf15fc404b3fb048390
THIEGO CARVALHO NASCIMENTO <thiego@rocketmail.com>
2025-05-13 19:35:10 -0300

    .nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-   base: '/',
+  base: '/',
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
