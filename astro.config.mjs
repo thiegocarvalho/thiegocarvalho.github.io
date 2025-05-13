481f9a496b43e480d740cde0aea2d3ae75aaf00b
THIEGO CARVALHO NASCIMENTO <thiego@rocketmail.com>
2025-05-13 19:14:09 -0300

    init

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+   base: '/',
+  integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/components",
+      },
+    },
+  },
+  output: "static",
+  build: {
+    inlineStylesheets: "auto",
+  },
+  server: {
+    host: true,
+    port: 4321,
+  },
+});
